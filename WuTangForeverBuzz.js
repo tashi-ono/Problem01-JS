b626e540e6fad65ba67e45f942355be4371373fa
Natasha Sana <nfsana@gmail.com>
2020-09-25 10:18:43 -0700

    push to git

--- WuTangForeverBuzz.js
+++ WuTangForeverBuzz.js
@@ -1,9 +1,24 @@
 class WuTangForeverBuzz {
-    wuTangClan(){
-        let output ="";
-
-        return output;
+  wuTangClan() {
+    let n = 15;
+    let output = "";
+    for (let i = 1; i <= n; i += 1) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        output += "WuTangForever\n";
+      } else if (i % 3 === 0) {
+        output += "Wu\n";
+      } else if (i % 5 === 0) {
+        output += "Tang\n";
+      } else {
+        output += i.toString() + "\n";
+      }
     }
+    return output;
+  }
 }
 
 module.exports = WuTangForeverBuzz;
+
+// console.log(wuTangClan(15));
+// .concat() is usually more efficient
+// refactor as a switch statement, which is more time efficient
--- wu.test.js
+++ wu.test.js
@@ -1,9 +1,10 @@
-const { TestScheduler } = require('jest');
-const WuTangForever = require('./WuTangForeverBuzz');
+const { TestScheduler } = require("jest");
+const WuTangForever = require("./WuTangForeverBuzz");
 
-test("The Clan is coming through....", () =>{
-    let theWu = new WuTangForever();
-    let expected = theWu.wuTangClan();
-    let actual = "1\n2\nWu\n4\nTang\nWu\n7\n8\nWu\nTang\n11\nWu\n13\n14\nWuTangForever\n";
-    expect(expected).toEqual(actual);
+test("The Clan is coming through....", () => {
+  let theWu = new WuTangForever();
+  let expected = theWu.wuTangClan();
+  let actual =
+    "1\n2\nWu\n4\nTang\nWu\n7\n8\nWu\nTang\n11\nWu\n13\n14\nWuTangForever\n";
+  expect(expected).toEqual(actual);
 });
